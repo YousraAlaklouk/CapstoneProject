311387407c1cec49fda78d77f2f2ceb915631476
Yousra Alaklouk <100789873+YousraAlaklouk@users.noreply.github.com>
2023-04-30 01:00:07 +0300

    history

---
+++ CapstoneProject/Controllers/SupervisorController.cs
@@ -0,0 +1,37 @@
+﻿using CapstoneProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Data;
+using Dapper;
+using System.Configuration;
+namespace CapstoneProject.Controllers
+{
+    public class SupervisorController : Controller
+    {
+        public ActionResult SupervisorInterface()
+        {
+            List < Models.Process> FriendList = new List<Models.Process>();
+            using (IDbConnection db = new SqlConnection("Data Source=DESKTOP-CNJT2HB\\SQLEXPRESS;Initial Catalog= MilkingSystem;Integrated Security=True"))
+            {
+
+                FriendList = db.Query<Models.Process>("SELECT* FROM MilkingProcess M INNER JOIN Person P ON P.PersonID= M.SupervisorID WHERE  M.Email = '" + Session["Email"].ToString() + "'").ToList();
+
+            }
+            return View(FriendList);
+        }
+
+        public ActionResult ProcessInformation()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ CapstoneProject/Models/Process.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc.Html;
+
+namespace CapstoneProject.Models
+{
+    public class Process
+    {
+
+        public int ProcessID { get; set; }
+
+        public int SupervisorID { get; set; }
+
+
+        public int AnimalID { get; set; }
+        public DateTime DateTime { get; set; }
+        public string State { get; set; }
+
+
+
+
+    }
+
+}
