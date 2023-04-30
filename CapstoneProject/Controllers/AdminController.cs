f1feabeb3716f9273487670aa7bbd457c77c6463
Yousra Alaklouk <100789873+YousraAlaklouk@users.noreply.github.com>
2023-04-30 21:43:32 +0300

    Admin File

---
+++ CapstoneProject/Controllers/AdminController.cs
@@ -0,0 +1,51 @@
+﻿using CapstoneProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Data;
+using Dapper;
+using System.Configuration;
+
+namespace CapstoneProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult AdminInterface()
+        {
+            return View();
+        }
+
+        public ActionResult SupervisorList()
+        {
+            List<Models.Process> FriendList = new List<Models.Process>();
+            using (IDbConnection db = new SqlConnection("Data Source=DESKTOP-CNJT2HB\\SQLEXPRESS;Initial Catalog= MilkingSystem;Integrated Security=True"))
+            {
+
+                FriendList = db.Query<Models.Process>("SELECT * FROM Person WHERE Role != CH'ADMIN'").ToList();
+
+            }
+            return View(FriendList);
+     }
+        public ActionResult ProcessHistory()
+        {
+            List<Models.Process> FriendList = new List<Models.Process>();
+            using (IDbConnection db = new SqlConnection("Data Source=DESKTOP-CNJT2HB\\SQLEXPRESS;Initial Catalog= MilkingSystem;Integrated Security=True"))
+            {
+
+                FriendList = db.Query<Models.Process>("SELECT * FROM MilkingProcess").ToList();
+
+            }
+            return View(FriendList);
+        }
+
+
+
+
+    }
+}
---
+++ CapstoneProject/Controllers/SettingsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapstoneProject.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public ActionResult ChangeSettings()
+        {
+            return View();
+        }
+
+
+
+    }
+}
