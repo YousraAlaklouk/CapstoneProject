513c6b44c3e094d7b28c604378569a8839fbb4ca
samaAlfares <s_alfares@yahoo.com>
2023-05-03 16:50:40 +0300

    Reset Password

--- CapstoneProject/Controllers/LoginController.cs
+++ CapstoneProject/Controllers/LoginController.cs
@@ -10,8 +10,9 @@ using System.Windows;
 using System.Windows.Forms;
 using System.Web.Mvc.Html;
 
-using CapstoneProject.Models;   
-
+using CapstoneProject.Models;
+using System.Net.Mail;
+using System.Net;
 
 namespace CapstoneProject.Controllers
 {
@@ -30,6 +31,61 @@ namespace CapstoneProject.Controllers
         public ActionResult SupervisorLogin()
         {
             return View();
+        }
+
+ 
+        public ActionResult ForgotPassword()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ForgotPassword(Enroll e)
+        {
+            Random random = new System.Random();
+            var value = "Aq" + random.Next(9999, 1000000) + "rs";
+
+            using (MailMessage mail = new MailMessage())
+            {
+               
+                mail.From = new MailAddress("samaalfares565@gmail.com");
+                mail.To.Add(e.Email);
+                mail.Subject = "Reset Password";
+                mail.Body = "Dear Customer, <br/><br/>Yor new Password is as follows:<br/></br><h2> " + value + "</h2><br/><br/>Thanks";
+                mail.IsBodyHtml = true;
+
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential("samaalfares565@gmail.com", "rleelwpahxpekdlw");
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                }
+            }
+
+            try
+            {
+                if (Request.HttpMethod == "POST")
+                {
+                    Enroll er = new Enroll();
+                    using (SqlConnection con = new SqlConnection("Data Source=DESKTOP-CNJT2HB\\SQLEXPRESS;Initial Catalog= MilkingSystem;Integrated Security=True"))
+                    {
+                        using (SqlCommand cmd = new SqlCommand("update Person set Password = '"+value+"' where Email = '"+e.Email+"'" , con))
+                        {
+                            con.Open();
+                            ViewData["result"] = cmd.ExecuteNonQuery();
+                            con.Close();
+                        }
+                    }
+                }
+                return View("AdminLogin");
+            }
+            catch (SqlException ee)
+            {
+                MessageBox.Show(ee.Message);
+                return View();
+            }
         }
 
         [HttpPost]
@@ -104,6 +160,5 @@ namespace CapstoneProject.Controllers
             return View("Home");
             //return new JsonResult { Data = new { status = status } };  
         }
-
     }
 }
